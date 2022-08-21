c93dcea744751bda19ce916af784141b42276616
dajhub <dajhub63@gmail.com>
2022-08-21 20:29:58 +0100

    install scripts updated

--- folders.sh
+++ folders.sh
@@ -12,9 +12,13 @@ echo "Creating folders for later use"
 [ -d $HOME"/.fonts" ] || mkdir -p $HOME"/.fonts"
 
 [ -d $HOME"/.config/geany" ] || mkdir -p $HOME"/.config/geany"
+[ -d $HOME"/.config/kitty" ] || mkdir -p $HOME"/.config/kitty"
+[ -d $HOME"/.config/openbox" ] || mkdir -p $HOME"/.config/openbox"
+[ -d $HOME"/.config/rofi" ] || mkdir -p $HOME"/.config/rofi"
+[ -d $HOME"/.config/sxhkd" ] || mkdir -p $HOME"/.config/sxhkd"
+[ -d $HOME"/.config/tint2" ] || mkdir -p $HOME"/.config/tint2"
 
-
-[ -d $HOME"/.local/share/themes" ] || mkdir -p $HOME"/.local/share/themes"
+[ -d $HOME"/.local/bin" ] || mkdir -p $HOME"/.local/bin"
 [ -d $HOME"/.local/share/xfce4/terminal/colorschemes" ] || mkdir -p $HOME"/.local/share/xfce4/terminal/colorschemes"
 [ -d $HOME"/.config/gtk-3.0" ] || mkdir -p $HOME"/.config/gtk-3.0"
 
@@ -22,3 +26,56 @@ echo "Creating folders for later use"
 echo "###############################################"
 echo "### Personal folders created or existed already"
 echo "###############################################"
+
+
+echo "Copy fonts to .fonts"
+
+cp -R ~/dotfiles-openbox/.fonts/* ~/.fonts/
+
+echo "Building new fonts into the cache files";
+echo "Depending on the number of fonts, this may take a while..."
+fc-cache -fv ~/.fonts
+
+
+echo "############################################"
+echo "#   Fonts have been copied and loaded       "
+echo "############################################"
+
+echo "Copy fonts to .icons"
+
+cp -R ~/dotfiles-openbox/.icons/* ~/.icons/
+
+echo "############################################"
+echo "#   Icons have been copied across           "
+echo "############################################"
+
+echo "Copy themes to .themes"
+
+cp -R ~/dotfiles-openbox/.themes/* ~/.themes/
+
+echo "############################################"
+echo "#   Themes have been copied across          "
+echo "############################################"
+
+echo "Copy .local/bin to .local/bin"
+
+cp -R ~/dotfiles-openbox/.local/bin* ~/.local/bin/
+
+echo "############################################"
+echo "#   Bin has been copied across              "
+echo "############################################"
+
+
+echo "Copy .config folders and files across to .config"
+
+cp -R ~/dotfiles-openbox/.config/geany* ~/.config/geany/
+cp -R ~/dotfiles-openbox/.config/kitty* ~/.config/kitty/
+cp -R ~/dotfiles-openbox/.config/openbox* ~/.config/openbox/
+cp -R ~/dotfiles-openbox/.config/rofi* ~/.config/rofi/
+cp -R ~/dotfiles-openbox/.config/sxhkd* ~/.config/sxhkd/
+cp -R ~/dotfiles-openbox/.config/tint2* ~/.config/tint2/
+
+
+echo "############################################"
+echo "#   .config folders and files have been copied across  "
+echo "############################################"
